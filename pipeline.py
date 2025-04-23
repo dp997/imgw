63c7b96d60cf773547099e8125729e8c61af6e16
dp997 <dominik.pierzga@gmail.com>
2025-04-23 16:24:25 +0200

    add switch for debug logging

--- pipeline.py
+++ pipeline.py
@@ -7,12 +7,13 @@ from imgw.utils import setup_logging
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("--local", action="store_true", help="Run pipeline locally (using duckdb)")
+    parser.add_argument("--verbose", action="store_true", help="Set logging level to DEBUG")
     args = parser.parse_args()
 
     setup_logging()
 
     logger = logging.getLogger()
-    # logger.setLevel("DEBUG")
+    logger.setLevel("DEBUG") if args.verbose else logger.setLevel("INFO")
     logger.info("Configuring DLT pipeline...")
 
     logger.info("Starting pipeline run...")
